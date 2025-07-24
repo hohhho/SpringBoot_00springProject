ad4142343b9b9036298972dbaa8b4027d885dd51
hohhho <andysh0701@gmail.com>
2025-07-24 12:35:15 +0900

    01. env, gradle 설정

--- src/main/java/com/myproject/springProject/SpringProjectApplication.java
+++ src/main/java/com/myproject/springProject/SpringProjectApplication.java
@@ -1,13 +1,19 @@
 package com.myproject.springProject;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SpringProjectApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication.run(SpringProjectApplication.class, args);
 	}
-
 }
